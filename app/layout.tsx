899f4d313c440e58660bdca42a56dea7a35b6dde
olesia <ldsolesya@gmail.com>
2025-11-24 15:02:10 +0000

    setup project structure and add favicon

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import TanStackProvider from "@/components/TanstackProvider/TanstackProvider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Car Rental App",
+  description: "A modern platform for renting cars easily and conveniently.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Car Rental App",
+    description:
+      "Find and rent the perfect car for any trip — quick, simple, reliable.",
+    url: "",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Car Rental App Promo",
+      },
+    ],
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <TanStackProvider>{children}</TanStackProvider>
+      </body>
+    </html>
+  );
+}
