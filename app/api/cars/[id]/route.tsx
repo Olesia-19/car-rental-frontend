19966d2927367dbe16ea4cc9b365aa57c40e59c2
olesia <ldsolesya@gmail.com>
2025-11-27 14:01:10 +0000

    edit API rout GET

--- app/api/cars/[id]/route.tsx
+++ app/api/cars/[id]/route.tsx
@@ -1,64 +1,20 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { api } from "@/app/api/api";
-// import { isAxiosError } from "axios";
-
-// interface Params {
-//   params: {
-//     id: string;
-//   };
-// }
-
-// export async function GET(req: NextRequest, context: Params) {
-//   try {
-//     const { id } = context.params;
-
-//     if (!id) {
-//       return NextResponse.json(
-//         { error: "Car ID is required" },
-//         { status: 400 }
-//       );
-//     }
-
-//     const res = await api.get(`/cars/${id}`);
-
-//     return NextResponse.json(res.data, { status: res.status });
-//   } catch (error) {
-//     if (isAxiosError(error)) {
-//       console.error("Axios error:", error.response?.data);
-//       return NextResponse.json(
-//         { error: error.message, response: error.response?.data },
-//         { status: error.response?.status ?? 500 }
-//       );
-//     }
-
-//     console.error("Unexpected error:", (error as Error).message);
-//     return NextResponse.json(
-//       { error: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
 import { NextRequest, NextResponse } from "next/server";
-import { api } from "../../api";
+import { api } from "@/app/api/api";
 import { isAxiosError } from "axios";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+type Params = {
+  params: Promise<{ id: string }>;
+};
+
+export async function GET(_req: NextRequest, { params }: Params) {
+  const { id } = await params;
+
+  if (!id) {
+    return NextResponse.json({ error: "Car ID is required" }, { status: 400 });
+  }
+
   try {
-    const { id } = params;
-
-    if (!id) {
-      return NextResponse.json(
-        { error: "Car ID is required" },
-        { status: 400 }
-      );
-    }
-
     const res = await api.get(`/cars/${id}`);
-
     return NextResponse.json(res.data, { status: res.status });
   } catch (error) {
     if (isAxiosError(error)) {
