bc9051b0db10a3c643cf9f1481fff05200e42599
olesia <ldsolesya@gmail.com>
2025-11-27 14:19:28 +0000

    edit api

--- app/api/api.ts
+++ app/api/api.ts
@@ -4,5 +4,5 @@ export type ApiError = AxiosError<{ error: string }>;
 
 export const api = axios.create({
   baseURL: `${process.env.HANDLER_API_URL}`,
-  withCredentials: true,
+  // withCredentials: true,
 });
